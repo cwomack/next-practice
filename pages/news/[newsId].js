6ce4e11a4c78c97cab363b7fa1c264643d8da6a3
WomackCodes <chris@womack.codes>
2021-08-03 01:15:53 -0700

    use NextJS Link rather than a tags to make SPA

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,16 @@
+import { useRouter } from 'next/router';
+
+
 //domain.com/news/[DynamicPath]  (Now a dynamic page for given ID's)
 
 function BreakingPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // send a request to the backend API to
+  // fetch the news item with the NewsId
+
   return <h1> The BREAKING News Page </h1>;
 }
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,24 @@
 //domain.com/news  (The landing page of news)
+import Link from 'next/link';
+import { Fragment } from "react";
 
-function HomePage() {
-    return <h1> The MAIN News Page </h1>
+function NewsPage() {
+    return (
+      <Fragment>
+        <h1> The MAIN News Page </h1>
+        <ul>
+          <li>
+            <Link href="/news/article1">Article 1</Link>
+          </li>
+          <li>
+            <Link href="/news/article2">Article 2</Link>
+          </li>
+          <li>
+            <Link href="/news/article3">Article 3</Link>
+          </li>
+        </ul>
+      </Fragment>
+    );
 }
 
-export default HomePage;
+export default NewsPage;
